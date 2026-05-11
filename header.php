6aea26625ab4b5edc22b68acce87df9a6b985edb
Gaurav Sharma <gsharmahit2012@gmail.com>
2026-05-10 21:42:22 -0700

    Keep public theme CSS last in head

--- header.php
+++ header.php
@@ -14,9 +14,9 @@
 	$boies_root_css = get_stylesheet_directory() . '/style.css';
 	$boies_asset_css = get_stylesheet_directory() . '/assets/css/anee.css';
 	?>
+	<?php wp_head(); ?>
 	<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>?ver=<?php echo esc_attr( file_exists( $boies_root_css ) ? filemtime( $boies_root_css ) : wp_get_theme()->get( 'Version' ) ); ?>">
 	<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/css/anee.css' ); ?>?ver=<?php echo esc_attr( file_exists( $boies_asset_css ) ? filemtime( $boies_asset_css ) : wp_get_theme()->get( 'Version' ) ); ?>">
-	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
